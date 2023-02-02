6b2b4f792c502b2d9f6a274848a1e824eb1f3dcd
slytherin20 <sonaligupta2904@gmail.com>
2023-02-02 13:51:07 +0530

    Assignment-3

--- App.js
+++ App.js
@@ -1,30 +1,32 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
-const h1 = React.createElement("h1",{className:"heading"},"Hello World");
-const ul = React.createElement("ul",{className:"fruits"},[
-    React.createElement("li",{
-        className:"apple",
-        key:"apple"  },
-        "Apple"),
-    React.createElement("li",{
-        className:"orange",
-        key:"orange"},
-        "Orange"),
-    React.createElement("li",{
-        className:"banana",
-        key:"banana"},
-        "Banana"),
-    React.createElement("li",{
-        className:"mango",
-        key:"mango"},
-        "Mango"),
-    React.createElement("li",{
-        className:"grapes",
-        key:"grapes"},
-        "Grapes")
-]);
-const container = React.createElement("div",{id:"container"},[
-    h1,ul
-])
+import Logo from "./logo.png";
+import UserIcon from "./user.png";
+// const header = React.createElement("div",{className:"title"},[
+//     React.createElement("h1",null,"heading1"),
+//     React.createElement("h2",null,"heading2"),
+//     React.createElement("h3",null,"heading3")
+// ])
+
+// const header = <div className="title">
+//     <h1>heading1</h1>
+//     <h2>Heading2</h2>
+//     <h3>Heading 3</h3>
+// </div>
+
+function Header(){
+    return <div className="header">
+       <img src={Logo} width="150" height="100" />
+       <input type="search" className="search" />
+       <img src={UserIcon} xwidth="40" height="40"/>
+     </div>
+}
+
+function Container(){
+    return <div className="container">
+        <Header />
+        <p>Container body</p>
+    </div>
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<Container />);
--- Readme.md
+++ Readme.md
@@ -1,1 +1,29 @@
-#### This repo includes all the React assignments.
+## Q: What is JSX?
+A: JSX is a syntax extension of javascript. It has all the super powers of JS and is used to create React Element. It syntax combines both markup and logic .
+<br>
+
+## Q: What are superpowers of JSX?
+A:
+ 1. Creates React elements by combining both markup and logic.
+ 2. Creates more readable, maintainable code.
+ 3. Easy debugging.
+ 4. Improves developer experience.
+ 5. Prevents XSS attacks.
+<br>
+
+## Q:  Role of type attribute in script? What are its different types?
+A: Type attribute helps browser identify the type of script it needs to execute.
+Different types:
+**Module:** Using this type makes script be treated as a module. Module scripts require use of CORS protocol for cross-fetching of data.
+**Classic script:**  Empty string passed in type, no type declared or a JS MIME type  makes the script be treated as a classic script.
+**Import map:** Its is a JSON object that developers can use to control how browsers resolve module specifiers when importing JS modules.
+**Data block:** The embedded content is treated as a data block and won’t be processed by browser. Developers must use a valid MIME type that is not javascript MIMI to indicate the data blocks.
+<br>
+
+## Q:  {TitleComp} vs {<TitleComp />} vs {<TitleComp></TitleComp>}
+A:
+ 1. {Title Comp} : React Element
+ 2. {<TitleComp />} : Self-enclosing React Component. Standard in the industry to call a component.
+ 3. {<TitleComp></TitleComp>} : React Component but not self-enclosing. Can use it but not usually preferred.
+
+
